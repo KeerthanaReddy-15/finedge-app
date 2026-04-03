32b5e0a5654b515b38e035d81dd718670801fbc8
KeerthanaReddy-15 <keerthanareddytholeti@gmail.com>
2026-04-03 12:21:08 +0530

    final wallet fix

---
+++ backend/controllers/walletController.js
@@ -0,0 +1,19 @@
+export const getBalance = (req, res) => {
+    res.send("Get balance working");
+};
+
+export const deposit = (req, res) => {
+    res.send("Deposit working");
+};
+
+export const transfer = (req, res) => {
+    res.send("Transfer working");
+};
+
+export const executeTrade = (req, res) => {
+    res.send("Execute trade working");
+};
+
+export const getTransactions = (req, res) => {
+    res.send("Get transactions working");
+};
